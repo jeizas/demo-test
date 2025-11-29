90fd94c948f995706a83723a62adb8e67b559c8c
jeizas <jeizas1210@gmail.com>
2025-11-29 21:47:09 +0800

    feat: 添加Lombok依赖并使用日志功能
    - 新增了Lombok依赖以简化代码
    - 在App类中集成了日志记录功能
    - 记录了不同级别的日志信息以便调试
    
    关联问题: #issue_number

--- src/main/java/com/jeizas/App.java
+++ src/main/java/com/jeizas/App.java
@@ -3,9 +3,17 @@ package com.jeizas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+        log.error("Hello, World!");
+        log.warn("Hello, World!");
+        log.info("Hello, World!");
+        log.debug("Hello, World!");
+        log.trace("Hello, World!");
     }
 }
